ece26edb3dd70da7c652d2884fe1ba442365fb43
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2022-07-19 17:57:44 +0200

    Add two Options Menu icons
    1) Search icon
    2) Setting icon

--- app/src/main/java/com/example/simpledayscounter/MainActivity.kt
+++ app/src/main/java/com/example/simpledayscounter/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.simpledayscounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.miSearch -> Toast.makeText(this, "sss", Toast.LENGTH_SHORT).show()
+            R.id.miSettings -> Toast.makeText(this, "sss", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
 }
+
+
+
+
