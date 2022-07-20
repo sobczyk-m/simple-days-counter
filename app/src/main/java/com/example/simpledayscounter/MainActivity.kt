18b09463ef980b460d78c9333d823f248d14cc07
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2022-07-20 11:01:32 +0200

    Add to fab Intent functionality

--- app/src/main/java/com/example/simpledayscounter/MainActivity.kt
+++ app/src/main/java/com/example/simpledayscounter/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.example.simpledayscounter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fun addCountdown() {
+            val intent = Intent(this, AddCountdownActivity::class.java)
+            startActivity(intent)
+        }
+
         val fabAddCountdown: View = findViewById(R.id.fabAddCountdown)
-        fabAddCountdown.setOnClickListener { view ->
-//            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null)
-//                .show()
+        fabAddCountdown.setOnClickListener {
+            addCountdown()
         }
 
     }
