b7f66a0121c75b70f4ff56d39a20693a560d09e8
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2022-07-20 12:37:45 +0200

    Add back button functionality  to action bar

--- app/src/main/java/com/example/simpledayscounter/AddCountdownActivity.kt
+++ app/src/main/java/com/example/simpledayscounter/AddCountdownActivity.kt
@@ -2,10 +2,21 @@ package com.example.simpledayscounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class AddCountdownActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_countdown)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> this.finish()
+        }
+        return true
     }
 }
