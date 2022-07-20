79dde5ca62b711381a10e80459518b36c4cfcf05
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2022-07-20 10:35:51 +0200

    Add floating action button icon

--- app/src/main/java/com/example/simpledayscounter/MainActivity.kt
+++ app/src/main/java/com/example/simpledayscounter/MainActivity.kt
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fabAddCountdown: View = findViewById(R.id.fabAddCountdown)
+        fabAddCountdown.setOnClickListener { view ->
+//            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null)
+//                .show()
+        }
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.top_app_bar_menu, menu)
         return true
     }
