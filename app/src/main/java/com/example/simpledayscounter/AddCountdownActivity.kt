48c2ef0868fb17307001279e07d91af27c5682cb
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2022-07-21 11:52:53 +0200

    Add to activity layout visibility conditions

--- app/src/main/java/com/example/simpledayscounter/AddCountdownActivity.kt
+++ app/src/main/java/com/example/simpledayscounter/AddCountdownActivity.kt
@@ -3,14 +3,46 @@ package com.example.simpledayscounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RadioButton
 
 class AddCountdownActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_countdown)
 
+        val rbDays = findViewById<RadioButton>(R.id.rbDays)
+        val rbWeeks = findViewById<RadioButton>(R.id.rbWeeks)
+        val rbMonths = findViewById<RadioButton>(R.id.rbMonths)
+        val rbYears = findViewById<RadioButton>(R.id.rbYears)
+
+        val llDayExclude = findViewById<LinearLayout>(R.id.llDayExclude)
+
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        fun ifRbDaysChecked() {
+            if (rbDays.isChecked) {
+                llDayExclude.visibility = View.VISIBLE
+            } else {
+                llDayExclude.visibility = View.GONE
+            }
+        }
+
+        rbDays.setOnClickListener {
+            ifRbDaysChecked()
+        }
+        rbWeeks.setOnClickListener {
+         ifRbDaysChecked()
+        }
+        rbMonths.setOnClickListener {
+            ifRbDaysChecked()
+        }
+        rbYears.setOnClickListener {
+            ifRbDaysChecked()
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
