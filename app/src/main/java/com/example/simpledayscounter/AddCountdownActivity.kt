29369754b17961b31d1ba7b05678063cfb4493ba
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2022-07-29 13:03:44 +0200

    Add calculation logic to DatePickerDialog

--- app/src/main/java/com/example/simpledayscounter/AddCountdownActivity.kt
+++ app/src/main/java/com/example/simpledayscounter/AddCountdownActivity.kt
@@ -12,7 +12,11 @@ import android.widget.RadioButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
+import java.text.SimpleDateFormat
 import java.util.*
+import java.time.LocalDate.now
+import java.time.LocalDate.of
+import java.time.Period.between
 
 class AddCountdownActivity : AppCompatActivity() {
 
@@ -28,6 +32,12 @@ class AddCountdownActivity : AppCompatActivity() {
     private var tvWdgEventName: TextView? = null
     private var tvWdgCountingText: TextView? = null
     private var tvWdgCountingNumber: TextView? = null
+
+    private var selectedDateStorage: String? = ""
+    private var differenceInDaysStorage: Int? = 0
+    private var differenceInWeeksStorage: Int? = 0
+    private var differenceInMonthsStorage: Int? = 0
+    private var differenceInYearsStorage: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -93,26 +103,46 @@ class AddCountdownActivity : AppCompatActivity() {
     }
 
     private fun showDatePicker() {
-
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
 
-        val etCountdownDate = findViewById<EditText>(R.id.etCountdownDate)
+        val dpd = DatePickerDialog(this, { _, mYear, mMonth, mDayOfMonth ->
 
-        val dpd = DatePickerDialog(
-            this,
-            { _, mYear, mMonth, mDayOfMonth ->
+            val selectedDate = "$mDayOfMonth/${mMonth + 1}/$mYear"
 
-                val selectedDate = "$mDayOfMonth/${mMonth + 1}/$mYear"
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
 
-                etCountdownDate.text = Editable.Factory.getInstance().newEditable(selectedDate)
+            val sdfSelectedDate = sdf.parse(selectedDate)
+            val selectedDateInDays = sdfSelectedDate.time / (1000 * 60 * 60 * 24)
+            val selectedDateInWeeks = sdfSelectedDate.time / (1000 * 60 * 60 * 24 * 7)
 
-            },
+            val sdfCurrentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+            val currentDateInDays = sdfCurrentDate.time / (1000 * 60 * 60 * 24)
+            val currentDateInWeeks = sdfCurrentDate.time / (1000 * 60 * 60 * 24 * 7)
+
+            val differenceInDays = selectedDateInDays - currentDateInDays
+            val differenceInWeeks = selectedDateInWeeks - currentDateInWeeks
+
+            val selectedLocaleDate = of(mYear, mMonth + 1, mDayOfMonth)
+            val currentLocaleDate = now()
+
+            val periodBetween = between(selectedLocaleDate, currentLocaleDate)
+            val differenceInMonths = periodBetween.months
+            val differenceInYears = periodBetween.years
+
+            selectedDateStorage = selectedDate
+            differenceInDaysStorage = differenceInDays.toInt()
+            differenceInWeeksStorage = differenceInWeeks.toInt()
+            differenceInMonthsStorage = differenceInMonths
+            differenceInYearsStorage = differenceInYears
+
+        },
             year,
             month,
             dayOfMonth
-        ).show()
+        )
+        dpd.show()
     }
 }
