5b11405fc1363d5f4f06208ed91a286ccbed4aac
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-12-19 19:32:43 +0100

    feat: update animation of changing screens

--- app/src/main/java/com/example/simpledayscounter/SimpleDaysCounterScreen.kt
+++ app/src/main/java/com/example/simpledayscounter/SimpleDaysCounterScreen.kt
@@ -1,14 +1,19 @@
 package com.example.simpledayscounter
 
 import androidx.annotation.StringRes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.simpledayscounter.presentation.counters.CountersScreen
 import com.example.simpledayscounter.presentation.add_counter.AddCounterScreen
+import com.example.simpledayscounter.presentation.counters.CountersScreen
+
 
 enum class SimpleDaysCounterScreen(@StringRes val title: Int) {
     Counters(title = R.string.app_name),
@@ -19,15 +24,43 @@ enum class SimpleDaysCounterScreen(@StringRes val title: Int) {
 fun SimpleDaysCounterApp(
     navController: NavHostController = rememberNavController()
 ) {
+    val screenWidth = LocalContext.current.resources.displayMetrics.widthPixels
+
     NavHost(
         navController = navController,
         startDestination = SimpleDaysCounterScreen.Counters.name,
         modifier = Modifier,
     ) {
-        composable(route = SimpleDaysCounterScreen.Counters.name) {
+        composable(route = SimpleDaysCounterScreen.Counters.name,
+            enterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { screenWidth.toInt() },
+                    animationSpec = tween(500)
+                )
+            },
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { -screenWidth.toInt() },
+                    animationSpec = tween(500)
+                )
+            }
+        ) {
             CountersScreen(navController = navController)
         }
-        composable(route = SimpleDaysCounterScreen.AddCounter.name) {
+        composable(route = SimpleDaysCounterScreen.AddCounter.name,
+            enterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { screenWidth.toInt() },
+                    animationSpec = tween(500)
+                )
+            },
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { -screenWidth.toInt() },
+                    animationSpec = tween(500)
+                )
+            }
+        ) {
             AddCounterScreen(navController = navController)
         }
     }
